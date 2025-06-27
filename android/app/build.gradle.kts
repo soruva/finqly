fa85af0072b028682a6a3fad7be44f5690051c96
SoruvaLab <soruvalab@gmail.com>
2025-06-27 12:26:34 +0000

    Fix NDK version and asset issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.fixed_project"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
