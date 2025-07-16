505381ae2f9d0895b7811e9eaad1fbcbb3ae4b27
SoruvaLab <soruvalab@gmail.com>
2025-07-16 14:20:51 +0000

    Update files and bump version code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.fixed_project"
+    namespace = "com.soruvalab.finqly"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -14,19 +13,16 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-
     defaultConfig {
         applicationId = "com.soruvalab.finqly"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        versionCode = flutter.versionCode    // pubspec.yaml の「+数字」と自動連動
+        versionName = flutter.versionName    // pubspec.yaml の「1.0.4」などと自動連動
     }
-
     signingConfigs {
         create("release") {
             storeFile = file(System.getenv("CM_KEYSTORE"))
@@ -35,15 +31,11 @@ android {
             keyPassword = System.getenv("CM_KEY_PASSWORD")
         }
     }
-
     buildTypes {
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
-            // isMinifyEnabled = false
-            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
-        getByName("debug") {
-        }
+        getByName("debug") {}
     }
 }
 
