fe854488daf8c6f0e0345437f3ce96ee1728a84e
SoruvaLab <soruvalab@gmail.com>
2025-08-02 12:15:25 +0900

    Update purchase_verification.dart

--- lib/services/purchase_verification.dart
+++ lib/services/purchase_verification.dart
@@ -1,7 +1,10 @@
 import 'package:in_app_purchase/in_app_purchase.dart';
 
 class PurchaseVerification {
+  // TODO: For production, implement server-side verification using Google Play Developer API.
+  // See: https://developer.android.com/google/play/billing/integrate#process
   static Future<bool> verifyPurchase(PurchaseDetails purchase) async {
+    // WARNING: This only checks local status. Server verification is required for secure in-app purchases.
     return purchase.status == PurchaseStatus.purchased;
   }
 }
