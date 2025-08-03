d832d653bf58e7f2417c84a50bd0f53452bf720e
SoruvaLab <soruvalab@gmail.com>
2025-08-03 23:03:58 +0900

    Update legal_webview_page.dart

--- lib/screens/legal_webview_page.dart
+++ lib/screens/legal_webview_page.dart
@@ -17,19 +17,66 @@ class LegalWebViewPage extends StatefulWidget {
 
 class _LegalWebViewPageState extends State<LegalWebViewPage> {
   late final WebViewController _controller;
+  bool _isLoading = true;
+  bool _hasError = false;
 
   @override
   void initState() {
     super.initState();
     _controller = WebViewController()
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (_) => setState(() {
+            _isLoading = true;
+            _hasError = false;
+          }),
+          onPageFinished: (_) => setState(() => _isLoading = false),
+          onWebResourceError: (_) => setState(() {
+            _isLoading = false;
+            _hasError = true;
+          }),
+        ),
+      )
       ..loadFlutterAsset(widget.assetPath);
+  }
+
+  Future<void> _goBack() async {
+    if (await _controller.canGoBack()) {
+      await _controller.goBack();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: WebViewWidget(controller: _controller),
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () async {
+            if (await _controller.canGoBack()) {
+              _goBack();
+            } else {
+              Navigator.of(context).maybePop();
+            }
+          },
+        ),
+      ),
+      body: Stack(
+        children: [
+          if (_hasError)
+            const Center(
+              child: Text(
+                'Failed to load page.',
+                style: TextStyle(color: Colors.red, fontSize: 18),
+              ),
+            )
+          else
+            WebViewWidget(controller: _controller),
+          if (_isLoading && !_hasError)
+            const Center(child: CircularProgressIndicator()),
+        ],
+      ),
     );
   }
 }
