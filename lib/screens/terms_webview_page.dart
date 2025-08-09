bbe09812b155f9b99cfb7cf8083a8acefc99f8ea
SoruvaLab <soruvalab@gmail.com>
2025-08-09 13:35:03 +0900

    Update terms_webview_page.dart

--- lib/screens/terms_webview_page.dart
+++ lib/screens/terms_webview_page.dart
@@ -1,6 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'dart:io';
+import 'package:url_launcher/url_launcher.dart';
 
 class TermsWebViewPage extends StatefulWidget {
   const TermsWebViewPage({super.key});
@@ -11,15 +12,47 @@ class TermsWebViewPage extends StatefulWidget {
 
 class _TermsWebViewPageState extends State<TermsWebViewPage> {
   late final WebViewController _controller;
+  int _progress = 0;
 
   @override
   void initState() {
     super.initState();
+
     if (Platform.isAndroid) {
       WebView.platform = SurfaceAndroidWebView();
     }
 
     _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.white)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (p) => setState(() => _progress = p),
+          onNavigationRequest: (request) async {
+            final uri = Uri.parse(request.url);
+
+            // Allow local asset
+            if (request.url.startsWith('file:///')) {
+              return NavigationDecision.navigate;
+            }
+
+            // Open external links with url_launcher (http/https/mailto/etc.)
+            if (uri.scheme == 'http' ||
+                uri.scheme == 'https' ||
+                uri.scheme == 'mailto' ||
+                uri.scheme == 'tel') {
+              final can = await canLaunchUrl(uri);
+              if (can) {
+                await launchUrl(uri, mode: LaunchMode.externalApplication);
+                return NavigationDecision.prevent;
+              }
+            }
+
+            // Default: block unknown schemes
+            return NavigationDecision.prevent;
+          },
+        ),
+      )
       ..loadFlutterAsset('assets/legal/terms.html');
   }
 
@@ -27,7 +60,13 @@ class _TermsWebViewPageState extends State<TermsWebViewPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Terms of Service')),
-      body: WebViewWidget(controller: _controller),
+      body: Column(
+        children: [
+          if (_progress > 0 && _progress < 100)
+            LinearProgressIndicator(value: _progress / 100),
+          Expanded(child: WebViewWidget(controller: _controller)),
+        ],
+      ),
     );
   }
 }
