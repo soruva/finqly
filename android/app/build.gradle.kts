4960966299fe726d9620d1bac620bd73e2306627
SoruvaLab <soruvalab@gmail.com>
2025-08-25 09:44:37 +0000

    chore: sync working tree

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,38 +8,40 @@ android {
     namespace = "com.soruvalab.finqly"
 
     compileSdk = 34
-    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         applicationId = "com.soruvalab.finqly"
-
         minSdk = maxOf(21, flutter.minSdkVersion)
-
         targetSdk = 34
-
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     signingConfigs {
-        create("release") {
-            storeFile = file(System.getenv("CM_KEYSTORE"))
-            storePassword = System.getenv("CM_KEYSTORE_PASSWORD")
-            keyAlias = System.getenv("CM_KEY_ALIAS")
-            keyPassword = System.getenv("CM_KEY_PASSWORD")
+        if (System.getenv("CM_KEYSTORE") != null) {
+            create("release") {
+                storeFile = file(System.getenv("CM_KEYSTORE"))
+                storePassword = System.getenv("CM_KEYSTORE_PASSWORD")
+                keyAlias = System.getenv("CM_KEY_ALIAS")
+                keyPassword = System.getenv("CM_KEY_PASSWORD")
+            }
         }
     }
     buildTypes {
         getByName("release") {
-            signingConfig = signingConfigs.getByName("release")
+            if (signingConfigs.findByName("release") != null) {
+                signingConfig = signingConfigs.getByName("release")
+            }
+            // isMinifyEnabled = true
+            // proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         getByName("debug") { }
     }
