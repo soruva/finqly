a31a9ed4bc066611980b1dc50601a83fd62f106d
SoruvaLab <soruvalab@gmail.com>
2025-09-04 03:59:07 +0000

    chore(gradle): clean up project-level build config

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+// android/build.gradle.kts（project-level）
+
 allprojects {
     repositories {
         google()
@@ -5,15 +7,14 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+
+    // project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
