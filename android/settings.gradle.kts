82f1a7d782430993e885c9c23ed981d56ccc9b50
SoruvaLab <soruvalab@gmail.com>
2025-09-04 04:21:29 +0000

    chore(gradle): robust settings.gradle with repo centralization and CI fallback

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,10 +1,16 @@
+// android/settings.gradle.kts
+
 pluginManagement {
     val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
+        val props = java.util.Properties()
+        val local = file("local.properties")
+        val envFlutter = System.getenv("FLUTTER_ROOT") // Codemagic 等のCI向けフォールバック
+        if (local.exists()) {
+            local.inputStream().use { props.load(it) }
+            props.getProperty("flutter.sdk") ?: envFlutter
+        } else {
+            envFlutter
+        } ?: error("flutter.sdk not set in local.properties and FLUTTER_ROOT not found")
     }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
@@ -16,10 +22,18 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.android.application") version "8.7.3" apply(false)
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply(false)
 }
 
 include(":app")
